0153768fa629bfbe1ca22e4712a190f4666a4f01
Thomas <thomas.nielsen@mail.utoronto.ca>
2025-09-09 12:47:38 -0400

    what

--- src/lab1/Main.java
+++ src/lab1/Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String[] args) {
         FizzBuzz.doFizzBuzz(5);
         FizzBuzz.main(args);
-        #im trying to test pull
+        //im trying to test pull
     }
 }
